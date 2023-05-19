1aa3d0e7d3041ffb9bb074919f81c63fa7712bb6
yzaya <ikuhle@yahoo.com>
2023-05-19 11:58:07 -0400

    will now collect links

--- CollectorGoog.py
+++ CollectorGoog.py
@@ -7,45 +7,32 @@ import sys, json, csv, re
 GNews = "https://news.google.com/news/rss/headlines"
 r = urllib.request\
     .urlopen(GNews)
-xml = r.read() #the data on the website is alrady in an xml format
+xml = r.read() #the data on the website is already in an xml format
 parser = "html.parser"
 soup = BeautifulSoup(xml,parser)
-soup # this is the parsed data
 
-# Parse the data into a dictionary for a json file
-'''
+# View an xml layout of the data being held in the soup variable
+# print(soup.prettify()) # 
+
+# Save it to an xml file
+# with open('Soup.xml', 'w') as f:
+#   f.write(str(soup.prettify()))
+
+# This works for getting the url, but is not needed:
+# for link in soup.find_all('source'):
+#    print(link.get('url'))
+
+# Turn the soup into a list
 results = []
 for item in soup.find_all("item"):
     title = item.find("title")
-    link = item.find("link")
+    link_element = item.find("link")
+    link = link_element.next_sibling.strip() if link_element and link_element.next_sibling else None
     pub_date = item.find("pubdate") or item.find("pubdate")
     if title is None:
         continue
     else:
-        result = {
-            "title": title.text,
-            "link": link.text,
-            "pub_date": pub_date.text
-        }
-        results.append(result)
-results
-
-# Append the data into a json file
-
-with open('googleNews2.json', '') as f:
-    json_list = f.read
-    json_list = json_list.append(json.dump(results,f))
-'''
-
-results = []
-for item in soup.find_all("item"):
-    title = item.find("title")
-    link = item.find("link")
-    pub_date = item.find("pubdate") or item.find("pubdate")
-    if title is None:
-        continue
-    else:
-        result = [title.text, link.text, pub_date.text]
+        result = [title.text, link, pub_date.text]
         results.append(result)
 results
 
@@ -53,7 +40,7 @@ results
 
 quoted_results = unquoted_data = [[re.sub(r'^"(.+)"$', r'\1', value) if col_index == 0 else value for col_index, value in enumerate(row)] for row in results]
 
-with open('googleNews.csv', "a", newline='') as f:
+with open('googleNewsTest.csv', "a", newline='') as f:
     w = csv.writer(f,delimiter=",", quotechar='"', quoting=csv.QUOTE_ALL)
     for row in quoted_results:
         w.writerow(row)
